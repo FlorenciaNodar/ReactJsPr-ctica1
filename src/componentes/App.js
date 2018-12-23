d23f9ba8102cf2a5a6a506f538766b8fb08ccea8
Florencia Nodar <fnodar@algeiba.com>
2018-12-23 15:59:30 -0300

    Se agregó una clase Helpers.js para crear funciones y llamarlas desde el componente Padre, para que sea más prolijo

--- src/componentes/App.js
+++ src/componentes/App.js
@@ -1,18 +1,36 @@
 import React, { Component } from 'react';
 import Header from './Header';
 import Formulario from './Formulario';
+import {obtenerDiferenciaAnio, calcularMarca, obtenerPlan} from '../helpers/Helpers'; //Así se llama a una función que la exporta un .js
+
 class App extends Component {
 
   cotizarSeguro = (datos) =>{
-     console.log(datos);
-    const {marca,plan,anio} = datos;
+    console.log(datos);
+    const {marca,plan,anio} = datos; //se carga el modelo que recibimos por parámetro en constantes de App.js
 
     //Agregar que la base de cada seguro sea de 2000
     let resultado = 2000;
 
+    //obtener la diferencia de años 
+    const diferenciaAnio = obtenerDiferenciaAnio(anio);
+    console.log('La diferencia es : ' + diferenciaAnio);
+    
     //por cada año el seguro es 3%  más barato. Obtener la diferencia de años y por cada año restar el 3%
+    resultado -=  ((diferenciaAnio * 3) * resultado) / 100;
+    console.log(resultado);
 
     //Si eligen auto europeo se incrementa 30%, asiatico 5%, americado 15%
+    resultado = calcularMarca(marca) * resultado;
+
+    //el plan del auto, el básico implementa el valor 20%, y el completo 50%
+    let incrementoPlan = obtenerPlan(plan);
+
+    //dependiente del plan incrementar
+    resultado = parseFloat(incrementoPlan * resultado).toFixed(); //para que sean solamente 2 dígitos
+
+
+    console.log(resultado);
   }
 
   render() {
