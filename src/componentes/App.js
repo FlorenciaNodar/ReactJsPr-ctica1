550d62df98a9ae5e1f29696817e7e7db05227873
Florencia Nodar <fnodar@algeiba.com>
2018-12-23 15:07:41 -0300

    Se agregó nuevo componente llamado Formulario, se exportó y se agregó en la pantalla principal App.js

--- src/componentes/App.js
+++ src/componentes/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import Header from './Header';
+import Formulario from './Formulario';
 class App extends Component {
   render() {
     return (
         <div className="contenedor">
-        <Header 
-          titulo = "Cotizador de Seguro de auto"
-        />
+            <Header
+              titulo = "Cotizador de Seguro de auto"/>
+        <div className="contenedor-formulario">
+            <Formulario />
         </div>
+        </div>
+       
     );
   }
 }
