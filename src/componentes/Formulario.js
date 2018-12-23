90aa20fc1a6165e5b1ea1c1b1ab9e84cb6d66667
Florencia Nodar <fnodar@algeiba.com>
2018-12-23 15:27:12 -0300

    Como leer datos de un formulario declarando las propiedades e implementarlas en dicho formulario

--- src/componentes/Formulario.js
+++ src/componentes/Formulario.js
@@ -1,12 +1,37 @@
 import React , {Component} from 'react';
 
 class Formulario extends Component {
+
+    //refs son para leer los valores de los campos de un formulario
+    marcaRef = React.createRef();
+    anioRef = React.createRef();
+    planBasicoRef = React.createRef();
+    planCompletoRef = React.createRef();
+
+    //También se pueden crear de otra manera las propiedades:
+    // Por el momento voy a dejarlas como estában anteriormente
+    // constructor(props){
+    //     super(props);
+    //     this.marcaRef = React.createRef();
+    //     this.anioRef = React.createRef();
+    //     this.planBasicoRef = React.createRef();
+    //     this.planCompletoRef = React.createRef();
+    // }
+
+    cotizarSeguro= (e) => { //Se implementa de ésta manera el método para no tener que escribir .bind(this) en los formularios
+        e.preventDefault(); // se usa para que no muestre en la url los campos del formulario            
+    //obtener los datos, crear el objeto y enviarlo al componente principal.
+    //hay que crear un refs por cada campo
+
+    console.log(this.marcaRef.current.value/*Se utiliza al current.value para acceder directamente al valor*/);
+    }
+
 render(){
     return(
-        <form className="cotizar-auto">
+        <form className="cotizar-auto" onSubmit={this.cotizarSeguro/*.bind(this)/*definimos .bind(this) para que se sepa que los atributos son de ésta clase y así poder llamarlos con this.*/} >
         <div className="campo">
             <label>Marca</label>
-            <select name="marca" >
+            <select name="marca" ref={this.marcaRef} >
                 <option value="americano">Americano</option>
                 <option value="europeo">Europeo</option>
                 <option value="asiatico">Asiatico</option>
@@ -15,7 +40,7 @@ render(){
 
         <div className="campo">
             <label>Año</label>
-            <select name="year">
+            <select name="year" ref={this.anioRef}>
                 <option value="2018">2018</option>
                 <option value="2017">2017</option>
                 <option value="2016">2016</option>
@@ -29,10 +54,10 @@ render(){
                 <option value="2008">2008</option>
             </select>
         </div>
-        <div className="campo">
+        <div className="campo"  >
             <label>Plan:</label>
-            <input type="radio" name="plan" value="basico"/> Básico
-            <input type="radio" name="plan" value="completo"/> Completo
+            <input type="radio" name="plan" value="basico" ref={this.planBasicoRef}/> Básico
+            <input type="radio" name="plan" value="completo" ref={this.planCompletoRef}/> Completo
         </div>
 
         <button type="submit" className="boton">Cotizar</button>
