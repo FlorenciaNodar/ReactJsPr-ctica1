b6a1fba37de2d0d3681cf8617363cb161a53d7c5
Florencia Nodar <fnodar@algeiba.com>
2018-12-23 14:59:57 -0300

    Se agregó el props en el componente header para poder enviarle el nombre que querramos en el componente padre que lo esta llamando

--- src/componentes/Header.js
+++ src/componentes/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = props=>{
     return(
         <header className="top">
-        <h1>Cotizador de seguro de auto</h1>
+        <h1>{props.titulo}</h1>
         </header>
     );
 }
